880458a1495cfdd0dff763cc9b36b966501306f5
tydonn <ariknurwantoh@gmail.com>
2026-07-07 11:47:27 +0700

    feat: add Respondent model with belongsTo relation

---
+++ app/Models/Quisioner/Respondent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Quisioner;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Respondent extends Model
+{
+    //
+    protected $connection = 'quisioner';
+    protected $table = 'dk_tbl_respondent';
+    protected $primaryKey = 'RespondentID';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'RespondentName',
+    ];
+}
